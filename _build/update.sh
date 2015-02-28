f75a6367176547f108813cbd7968ea63cd66c5d4
kodmunki <kodmunki@gmail.com>
2015-02-28 13:27:13 -0600

    Update script

--- _build/update.sh
+++ _build/update.sh
@@ -6,7 +6,10 @@ cp -f ../bin/ku4js-reflection.js ../../ku4js-reflection/tests/_dependencies/
 cp -f ../bin/ku4js-reflection.js ../../ku4js-workers/tests/_dependencies/
 cp -f ../bin/ku4js-reflection.js ../../ku4js-workers/example/scripts/example/lib/
 cp -f ../bin/ku4js-reflection.js ../../ku4js-webApp/tests/_dependencies/
-cp -f ../bin/ku4js-reflection.js ../../ku4js-webApp/example/scripts/example/lib/
+cp -f ../bin/ku4js-reflection.js ../../ku4js-webApp/_example/javascript/lib/
 cp -f ../bin/ku4js-reflection.js ../../ku4js-webApp/_TEMPLATE/lib/
+
+echo "Copying resources to _LATEST/"
+cp -f ../bin/ku4js-reflection.js ../../_LATEST/
 
 echo "Update complete :{)}"
